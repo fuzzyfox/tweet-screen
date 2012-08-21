aefda4af1d414d95724e1c3031d6dce817cb8535
William Duyck <fuzzyfox0@gmail.com>
2012-08-21 21:42:45 +0100

    added better request function

--- proxy.php
+++ proxy.php
@@ -19,6 +19,20 @@
 		
 		private $_OAuth;
 		private $_params = array(array());
+		private $_data = array();
+		private $_response_codes = array(
+			304 => 'There was no new data to return.',
+			400 => 'The request was invalid.',
+			401 => 'Authentication credentials were missing or incorrect.',
+			403 => 'The request is understood, but it has been refused.',
+			404 => 'The URI requested is invalid or the resource requested does not exists.',
+			406 => 'Invalid format is specified in the request.',
+			420 => 'Rate limit reached.',
+			500 => 'Something is broken.',
+			502 => 'Twitter is down or being upgraded.',
+			503 => 'Twitter is over capacity.',
+			504 => 'Gateway timeout.'
+		);
 		
 		/**
 		 * sets up the oauth session
@@ -40,23 +54,44 @@
 			);
 		}
 		
+		/**
+		 * set a param for the search
+		 *
+		 * @param	string	key		the name of the param
+		 * @param	string	value	the value of the param
+		 */
 		public function set_param($key, $value) {
 			$this->_params[$key] = $value;
 		}
 		
+		/**
+		 * Sends a request to the twitter search api, using params set in object
+		 *
+		 * @return	boolean	TRUE on success
+		 */
 		public function request() {
-			return $this->_OAuth->request('GET', 'http://search.twitter.com/search.json', $args, FALSE);
+			$this->_OAuth->request('GET', 'http://search.twitter.com/search.json', $args, FALSE);
+			
+			if($this->_OAuth->response['code'] == 200)
+			{
+				$data = json_decode($this->_OAuth->response['response']);
+				return TRUE;
+			}
+			else
+			{
+				header($this->_response_codes[$this->_OAuth->response['code']], TRUE, $this->_OAuth->response['code']);
+				return FALSE;
+			}
 		}
 		
 		/**
-		 * Generates a qrcode using the google charts api
-		 *
 		 * Generates a qrcode to tweet a specified message out.
 		 *
 		 * @param	string	msg	the message to encode
 		 */
 		public function generate_qr($msg) {
-			
+			$tweet_url = 'https://twitter.com/home?status=' . urlencode($msg);
+			return 'https://chart.googleapis.com/chart?cht=qr&chs=170x170&chl=' . urlencode($tweet_url) . '&chld=H|1';
 		}
 		
 	}
@@ -155,10 +190,24 @@
 
 	// generate qrcode for tweet out
 	
-	// expected urls
 	/*
+	 Tweets:
+	 should return tweets in json, with urls replaced for the cached versions
 	 ?hashtag=%23mozcamp
 	 ?hashtag=%23mozcamp&since_id=12243
+	 
+	 QR:
+	 should return the url for cached version of the qrcode, and the message that
+	 will be tweeted
+	 ?qr=message+to+tweet
+	 
+	 Promos:
+	 no id: return the total number of snippets in the snippets dir, id's map
+	 to filenames in alphabetical order
+	 ?promo=0
+	 
+	 with id: return the relative url and ID selector of the snippet for the promo
+	 ?promo=3
 	*/
 
 // EOF
